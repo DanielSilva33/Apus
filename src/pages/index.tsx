f42c5b0bd8304603e313bcbc60b302e0b4cf0b99
DanielSilva <daniel.silva3320@gmil.com>
2022-12-15 18:38:35 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Apus</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>
+          Empresa ainda sem nome
+        </h1>
+      </main>
+    </>
+  )
+}
