fdb609b6366d5c2629f74e8ef94c5bb3977079ab
DanielSilva <daniel.silva3320@gmil.com>
2023-01-24 11:53:43 -0300

    add image

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import Logo from '../../public/logo.svg';
 
 export default function Header() {
   return (
     <>
       <header className='bg-slate-900 rounded-sm opacity-90 h-10 w-auto'>
+        <Image
+          src={Logo}
+          className='w-14'
+          alt='Imagem ilustrativa Logo Risi Consultoria'
+        />
         <div className='text-white text-center pt-2 text-base space-x-5'>
           <Link href='/'>Inicio</Link>
           <Link href='/sobre'>Sobre</Link>
